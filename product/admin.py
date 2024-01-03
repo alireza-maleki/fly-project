861b5fe14af080ac43672d15ec64a55a27899f6d
AZUREDEVOPS/a.maleki <alirezamalekinia11@gmail.com>
2024-01-03 14:33:13 +0330

    feat(ui) : edit model & admin file in product

--- product/admin.py
+++ product/admin.py
@@ -1,28 +1,37 @@
 from django.contrib import admin
 
-from product.models import Book, Category, Publisher
+# from product.models import Book, Category, Publisher
 
 
-class BookCategoriesInline(admin.StackedInline):
-    model = Book.categories.through
+# class BookCategoriesInline(admin.StackedInline):
+#     model = Book.categories.through
 
 
-class BookPublisherInline(admin.StackedInline):
-    model = Book
+# class BookPublisherInline(admin.StackedInline):
+#     model = Book
 
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'create_date', 'last_update')
-    search_fields = ('title', 'year')
-    readonly_fields = ('last_update',)
+# @admin.register(Book)
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'create_date', 'last_update')
+#     search_fields = ('title', 'year')
+#     readonly_fields = ('last_update',)
 
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    inlines = [BookCategoriesInline]
+# @admin.register(Category)
+# class CategoryAdmin(admin.ModelAdmin):
+#     inlines = [BookCategoriesInline]
 
 
-@admin.register(Publisher)
-class PublisherAdmin(admin.ModelAdmin):
-    inlines = [BookPublisherInline]
+# @admin.register(Publisher)
+# class PublisherAdmin(admin.ModelAdmin):
+#     inlines = [BookPublisherInline]
+
+
+from product.models import FlightTicket
+
+@admin.register(FlightTicket)
+class FlightTicketAdmin(admin.ModelAdmin):
+    list_display = ('passenger_name', 'passenger_email', 'departure_city', 'destination_city', 'departure_date', 'arrival_date', 'ticket_number', 'ticket_price', 'created_at')
+    search_fields = ('passenger_name', 'ticket_number')
+    readonly_fields = ('created_at',)
--- product/models.py
+++ product/models.py
@@ -1,46 +1,24 @@
 from django.db import models
 
+class FlightDestination(models.Model):
+    city = models.CharField(max_length=100, verbose_name='شهر')
 
-# class Publisher(models.Model):
-#     title = models.CharField(max_length=31, blank=False, verbose_name='نام')
-
-
-# class Category(models.Model):
-#     title = models.CharField(max_length=31, blank=False, verbose_name='نام')
-
-
-# class Book(models.Model):
-#     title = models.CharField(max_length=31, blank=False, verbose_name='نام')
-#     create_date = models.DateField(null=True, blank=True, verbose_name='تاریخ ایجاد')
-#     last_update = models.DateTimeField(null=True, blank=True)
-#     categories = models.ManyToManyField(Category, related_name='books', blank=False)
-#     publisher = models.ForeignKey(Publisher, related_name='books', null=True, on_delete=models.SET_NULL)
-#     price = models.IntegerField(null=True, blank=True, verbose_name='قیمت')
-
-#     def __str__(self):
-#         return f"{self.title} - {self.create_date}"
-
-
-# در فایل models.py
-
-from django.db import models
+    def __str__(self):
+        return self.city
 
 class FlightTicket(models.Model):
-    # اطلاعات مسافر
     passenger_name = models.CharField(max_length=100, verbose_name='نام مسافر')
     passenger_email = models.EmailField(verbose_name='ایمیل مسافر')
 
-    # اطلاعات پرواز
-    departure_city = models.CharField(max_length=100, verbose_name='شهر حرکت')
-    destination_city = models.CharField(max_length=100, verbose_name='شهر مقصد')
+    departure_cities = models.ManyToManyField(FlightDestination, related_name='departure_tickets', verbose_name='شهر حرکت')
+    destination_cities = models.ManyToManyField(FlightDestination, related_name='destination_tickets', verbose_name='شهر مقصد')
+
     departure_date = models.DateField(verbose_name='تاریخ حرکت')
     arrival_date = models.DateField(verbose_name='تاریخ ورود')
 
-    # اطلاعات بلیط
     ticket_number = models.CharField(max_length=20, unique=True, verbose_name='شماره بلیط')
     ticket_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='قیمت بلیط')
 
-    # اطلاعات اضافی
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
 
     def __str__(self):
@@ -49,15 +27,3 @@ class FlightTicket(models.Model):
     class Meta:
         verbose_name = 'بلیط هواپیما'
         verbose_name_plural = 'بلیط‌های هواپیما'
-
-
-    # def clean(self):
-    #     if self.price > 10000:
-    #         super(Book, self).clean()
-    #     else:
-    #         raise ValidationError('price > 10.000')
-
-    # def save(self, *args, **kwargs):
-    #     if self.price < 2000:
-    #         raise ValueError('price < 2000')
-    #     super(Book, self).save()
